064f2693ff9c5b685025fa09e2f9e4ecde9ecfb2
Cyber Punk <cyberp2000s@gmail.com>
2025-09-07 10:53:05 +0530

    Update entrypoint-tailscale.sh

--- entrypoint-tailscale.sh
+++ entrypoint-tailscale.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 set -e
 
-# Start tailscaled in userspace networking mode
-tailscaled --state=/tmp/tailscaled.state --tun=userspace-networking &
+# Start tailscaled in userspace mode
+/usr/local/bin/tailscaled --state=/tmp/tailscaled.state --tun=userspace-networking &
 
-# Give tailscaled a moment to start
+# Wait for tailscaled to be ready
 sleep 5
 
-# Bring up Tailscale client with your auth key
-tailscale up \
+# Run tailscale up with the auth key from environment
+/usr/local/bin/tailscale up \
   --authkey=${TAILSCALE_AUTHKEY} \
   --hostname=render-node \
   --accept-routes \
   --accept-dns=false
 
-# Keep container running
-wait -n
+# Keep the container alive
+tail -f /dev/null
